25701e824f5fa8dea875fb2932110ea7554c17f5
Saray Mendivelso <zaryz2005@gmail.com>
2024-11-07 06:16:28 -0500

    Proyecto Funcional

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import UserNameInput from './components/UserNameInput';
+import CreateGame from './components/CreateGame';
+import GameList from './components/GameList';
+import Lobby from './components/Lobby';
+import TriviaGame from './components/TriviaGame';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [userName, setUserName] = useState('');
+    const [isUserNameSet, setIsUserNameSet] = useState(false);
+    const [selectedGame, setSelectedGame] = useState(null);
+    const [gameStarted, setGameStarted] = useState(false);
+
+    const handleUserNameSubmit = (name) => {
+        setUserName(name);
+        setIsUserNameSet(true);
+    };
+
+    const handleGameSelection = (gameName) => {
+        setSelectedGame(gameName);
+    };
+
+    const handleStartGame = () => {
+        setGameStarted(true);
+    };
+
+    return (
+        <div>
+            {!isUserNameSet ? (
+                <UserNameInput onSubmit={handleUserNameSubmit} />
+            ) : (
+                <div>
+                    {!selectedGame ? (
+                        <>
+                            <CreateGame userName={userName} />
+                            <GameList userName={userName} onSelectGame={handleGameSelection} />
+                        </>
+                    ) : !gameStarted ? (
+                        <Lobby gameName={selectedGame} userName={userName} onStartGame={handleStartGame} />
+                    ) : (
+                        <TriviaGame gameName={selectedGame} userName={userName} />
+                    )}
+                </div>
+            )}
+        </div>
+    );
 }
 
 export default App;
